46041b0a12eac0f413c1203a19cebdf46c0dbd6c
Klyrasel <KaiLyrasel@outlook.com>
2022-12-05 19:44:31 -0500

    snapshot

---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+import renderer from 'react-test-renderer'
+
+test('renders a snapshot', () => {
+  const tree = renderer.create(<App/>).toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
